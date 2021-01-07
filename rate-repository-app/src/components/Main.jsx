ea619356e04c1349017b362da394726d6ec9f027
JTTran <tranjt80>
2021-01-07 03:53:07 +0200

    10.3: the reviewed repositories list

---
+++ rate-repository-app/src/components/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Constants from 'expo-constants';
+import { Text, StyleSheet, View } from 'react-native';
+import RepositoryList from './RepositoryList';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+  title: {
+    marginBottom: 20
+  }
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Rate Repository Application</Text>
+      <RepositoryList />
+    </View>
+  );
+};
+
+export default Main;
