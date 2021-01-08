21a7d5084a31c6aa428d2218c8e307a2f867aa9c
JTTran <tranjt80>
2021-01-08 10:33:49 +0200

    10.7: scrollable app bar

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
 
 const styles = StyleSheet.create({
-  container: {    
-    paddingTop: Constants.statusBarHeight,
-    flex: 0,
+  container: {
+    paddingTop: Constants.statusBarHeight,    
     backgroundColor: theme.colors.appBar,
+  },
+  scrollView: {
     flexDirection: 'row',
-    alignItems: 'flex-end',    
+    alignItems: 'flex-end',
   }
 });
 
 const AppBar = () => {
-  return <View style={styles.container}>
-    <AppBarTab text='Repositories' link={'/'} />
-    <AppBarTab text='Sign In' link={'/signin'} />
-  </View>;
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal>
+        <AppBarTab text='Repositories' link={'/'} />
+        <AppBarTab text='Sign In' link={'/signin'} />      
+      </ScrollView>
+    </View>
+  );
 };
 
 export default AppBar;
