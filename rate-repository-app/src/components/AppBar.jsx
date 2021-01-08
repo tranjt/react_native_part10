6e6b8676fdaab6fd8d9f50eec2e1a0b5188c47dd
JTTran <tranjt80>
2021-01-08 03:02:27 +0200

    10.4: the app bar

---
+++ rate-repository-app/src/components/AppBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import theme from '../theme';
+import AppBarTab from './AppBarTab';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+    backgroundColor: theme.colors.appBar,
+    flexDirection: "row",
+    alignItems: "flex-end",    
+  }
+});
+
+const AppBar = () => {
+  return <View style={styles.container}>
+    <AppBarTab text="Repositories" />
+  </View>;
+};
+
+export default AppBar;
---
+++ rate-repository-app/src/components/AppBarTab.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import Text from './Text';
+
+const styles = StyleSheet.create({  
+  text: {
+    color: "white",
+    margin: 15,
+  }
+});
+
+const AppBarTab = ({ text }) => {
+  return <TouchableWithoutFeedback style={styles.container}>
+    <Text fontWeight="bold" fontSize="title" style={styles.text}>{text}</Text>
+  </TouchableWithoutFeedback>;
+};
+
+export default AppBarTab;
