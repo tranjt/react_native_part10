dacbb77871f579a55c545589ef9cdd6559f18426
JTTran <tranjt80>
2021-01-09 05:18:46 +0200

    10.10: a platform specific font

--- rate-repository-app/src/theme.js
+++ rate-repository-app/src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -12,7 +14,11 @@ const theme = {
     title: 20,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System',
+    }),
   },
   fontWeights: {
     normal: '400',
